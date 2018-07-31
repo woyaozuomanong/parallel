12c3730b41a953871a3852cb8e1bfc804f6b69cb
leinb <leinb@cnpe.cc>
2018-07-31 22:12:49 +0800

    add the input function, and change f(x) from sin(x) to x

--- mpitrape.c
+++ mpitrape.c
@@ -9,18 +9,19 @@
 #include<mpi/mpi.h>
 #include<math.h>
 double Trap(double left_endpt,double right_endpt,int trap_count,double base_len);
+void Get_input(int my_rank,int comm_sz,double *a_p,double *b_p,int *n_p);
 
 int main(void)
 {
-    int my_rank,comm_sz,n=1024,local_n;
-    double a=0,b=3.14,h,local_a,local_b;
+    int my_rank,comm_sz,n,local_n;
+    double a,b,h,local_a,local_b;
     double local_int,total_int;
     int source;
 
     MPI_Init(NULL,NULL);
     MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
     MPI_Comm_size(MPI_COMM_WORLD,&comm_sz);
-
+    Get_input(my_rank,comm_sz,&a,&b,&n);
     h=(b-a)/n;
     local_n=n/comm_sz;
 
@@ -58,13 +59,38 @@ double Trap(double left_endpt,double right_endpt,int trap_count,double base_len)
     double estimate,x;
     int i;
 
-    estimate=(sin(left_endpt)+sin(right_endpt))/2.0;
+    estimate=(left_endpt+right_endpt)/2.0;
     for(i=1;i<=trap_count-1;i++)
     {
         x=left_endpt+i*base_len;
-        estimate+=sin(x);
+        estimate+=x;
     }
     estimate=estimate*base_len;
 
     return estimate;
+}
+
+
+
+void Get_input(int my_rank,int comm_sz,double *a_p,double *b_p,int *n_p)
+{
+    int dest;
+
+    if(my_rank==0)
+    {
+        printf("Enter a,b and n\n");
+        scanf("%lf %lf %d",a_p,b_p,n_p);
+        for(dest=1;dest<comm_sz;dest++)
+        {
+            MPI_Send(a_p,1,MPI_DOUBLE,dest,0,MPI_COMM_WORLD);
+            MPI_Send(b_p,1,MPI_DOUBLE,dest,0,MPI_COMM_WORLD);
+            MPI_Send(n_p,1,MPI_INT,dest,0,MPI_COMM_WORLD);
+        } 
+    }
+    else
+    {
+        MPI_Recv(a_p,1,MPI_DOUBLE,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+        MPI_Recv(b_p,1,MPI_DOUBLE,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+        MPI_Recv(n_p,1,MPI_INT,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+    }
 }
